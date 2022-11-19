1e10942e5b01d55d9e591cfa4cca2297bf50b761
alexjenkinsynwa@gmail.com <alexjenkins@Alexs-MacBook-Pro.local>
2022-11-19 00:45:49 -0500

    updated client

--- client/src/client.js
+++ client/src/client.js
@@ -1,14 +1,15 @@
-import sanityClient from '@sanity/client'
-import imageUrlBuilder from '@sanity/image_url';
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
-  apiVersion: '2021-11-1',
-  useCdn: false,
-  toke: process.env.REACT_APP_SANITY_TOKEN,
+  apiVersion: '2021-11-16',
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);
+
--- client/src/components /Login.js
+++ client/src/components /Login.js
@@ -5,20 +5,30 @@ import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
 
+import { client } from '../client';
+
 const Login = () => {
+  const navigate = useNavigate();
   const responseGoogle = (response) => {
     localStorage.setItem('user', JSON.stringify(response.profileObj));
-    
+
     const { name, googleId, imageUrl} = response.profileObj;
 
+    //sanity user schema 
     const doc = {
       _id: googleId,
       _type: 'user',
       userName: name,
       image: imageUrl,
-    };
+    }
+
+    //if user does not exist, will 
+    client.createIfNotExists(doc)
+    .then(() => {
+      navigate ('/', { replace: true })
+    })
    
-  };
+  }
 
   return (
     
